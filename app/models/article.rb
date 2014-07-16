dbbf9a6831bcfcb293e7c0db254ba4cf5b4541f9
Franklyn <zfranklyn@gmail.com>
2014-07-16 17:25:17 -0400

    2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true,
                     length: { minimum: 5 }
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 end
