69c4e79aa3a203c448cdbca5fa7097c6cce76d56
Franklyn <zfranklyn@gmail.com>
2014-07-24 15:07:00 -0400

    make basic user model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720204639) do
+ActiveRecord::Schema.define(version: 20140724185937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,12 +35,13 @@ ActiveRecord::Schema.define(version: 20140720204639) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
